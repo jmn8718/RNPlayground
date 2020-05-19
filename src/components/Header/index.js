0c5aebff6462e9dc33ae8a5962f2a25024f4b009
Jose <jose@plingo.io>
2020-05-19 15:56:33 +0900

    handle logout from header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,18 +1,59 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useContext } from 'react';
+import { View, Platform, TouchableHighlight, StyleSheet } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import Icon from 'react-native-vector-icons/Ionicons';
 
-import { styles } from '../../styles';
+import { styles as appStyles } from '../../styles';
+import { AppStoreContext } from '../../contexts';
+import auth from '../../services/auth';
 
-export const MainHeader = function () {
-  const insets = useSafeAreaInsets();
+const UserIcon = function ({ user, onPress = () => {} }) {
+  const iconName = user ? 'log-out' : 'log-in';
   return (
-    <View style={{ ...styles.mainHeader, paddingTop: insets.top }}>
-      <View style={styles.mainHeaderContent}>
-        <View style={styles.mainHeaderLeft}>{/* SET CONTENT */}</View>
+    <TouchableHighlight style={styles.iconRightWrapper} onPress={onPress}>
+      <Icon
+        name={`${Platform.OS === 'ios' ? 'ios' : 'md'}-${iconName}`}
+        size={30}
+        color="black"
+      />
+    </TouchableHighlight>
+  );
+};
+
+export const MainHeader = function ({ navigation }) {
+  const { state, dispatch } = useContext(AppStoreContext);
+  const insets = useSafeAreaInsets();
+
+  const logout = async () => {
+    dispatch({
+      type: 'CLEAR_USER',
+    });
+    await auth.logout(state.provider);
+  };
+  const goToLogin = () => {
+    navigation.navigate('Login');
+  };
+
+  return (
+    <View style={{ ...appStyles.mainHeader, paddingTop: insets.top }}>
+      <View style={appStyles.mainHeaderContent}>
+        <View style={appStyles.mainHeaderLeft}>{/* SET CONTENT */}</View>
         {/* <Text> TITLE </Text> */}
-        <View style={styles.mainHeaderRight}>{/* SET CONTENT */}</View>
+        <View style={appStyles.mainHeaderRight}>
+          <UserIcon
+            user={state.user}
+            onPress={state.user ? logout : goToLogin}
+          />
+        </View>
       </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  iconRightWrapper: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
